3def4d067ae569daee684d0fb5097cd7f0c3f0c6
bhanuprakash9980 <bhanuprakashshettigar@gmail.com>
2020-08-25 01:05:30 +0530

    rectifird

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       {...rest}
       render={(props) =>
         !isAuthenticated && !loading ? (
-          <Redirect to='/Login' />
+          <Redirect to='/login' />
         ) : (
           <Component {...props} />
         )
